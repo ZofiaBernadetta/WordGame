99dfd4cabf8ba8b0897c0d7cd7e8a303c83ece54
Zofia Drabek <zosia.drabek@gmail.com>
2020-07-20 08:00:32 +0200

    Add more tests.

--- WordGameTests/GameTests.swift
+++ WordGameTests/GameTests.swift
@@ -22,5 +22,66 @@ class GameTests: XCTestCase {
         XCTAssertEqual(game.allWords.count, 297)
     }
     
+    func testDidntChooseWord() {
+        let word = game.currentWord.spa
+        
+        let currentResult = game.currentResult
+        let lives = game.numberOfLives
+        let text = game.didntChooseWord(word: word)
+        
+        XCTAssertEqual(text, "-1")
+        XCTAssertEqual(currentResult - 1, game.currentResult)
+        XCTAssertEqual(lives - 1, game.numberOfLives)
+        
+        let newWord = game.currentWord.spa
+        let differentWord: String = {
+            var word = ""
+            repeat {
+                word = game.allWords.randomElement()?.spa ?? ""
+            } while word == newWord
+            return word
+        }()
+        
+        let lives1 = game.numberOfLives
+        let currentResult1 = game.currentResult
+        let newText = game.didntChooseWord(word: differentWord)
+        
+        XCTAssertEqual(newText, "")
+        XCTAssertEqual(lives1, game.numberOfLives)
+        XCTAssertEqual(currentResult1, game.currentResult)
+        
+        let newWord1 = game.currentWord.spa
+        let newText1 = game.didntChooseWord(word: newWord1)
+        
+        XCTAssertEqual(newText1, "game over")
+    }
     
+    func testSelectedWord() {
+        let word = game.currentWord.spa
+        
+        let currentResult = game.currentResult
+        let lives = game.numberOfLives
+        let text = game.selectedWord(word)
+        
+        XCTAssertEqual(text, "+1")
+        XCTAssertEqual(currentResult + 1, game.currentResult)
+        XCTAssertEqual(lives, game.numberOfLives)
+        
+        let newWord = game.currentWord.spa
+        let differentWord: String = {
+            var word = ""
+            repeat {
+                word = game.allWords.randomElement()?.spa ?? ""
+            } while word == newWord
+            return word
+        }()
+        
+        let lives1 = game.numberOfLives
+        let currentResult1 = game.currentResult
+        let newText = game.didntChooseWord(word: differentWord)
+        
+        XCTAssertEqual(newText, "-1")
+        XCTAssertEqual(lives1 - 1, game.numberOfLives)
+        XCTAssertEqual(currentResult1 - 1, game.currentResult)
+    }
 }
